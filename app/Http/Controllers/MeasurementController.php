e3860e313eddbeecfc904b3a9af3209ecc523d60
DanielHajduk98 <danhajduk@protonmail.com>
2021-03-10 18:04:58 +0100

    :tada: Initial commit.
    Contains simple api.

---
+++ app/Http/Controllers/MeasurementController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Measurement;
+use Illuminate\Http\Request;
+
+class MeasurementController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return int
+     */
+    public function store()
+    {
+        Measurement::create(request(['temperature', 'humidity', 'air_pressure', 'movement', 'luminosity']));
+
+        return 200;
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Measurement  $measurement
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Measurement $measurement)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Measurement  $measurement
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Measurement $measurement)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Measurement  $measurement
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Measurement $measurement)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Measurement  $measurement
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Measurement $measurement)
+    {
+        //
+    }
+}
