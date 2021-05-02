f23895c287af212d79f3303a00107fe278f200f8
DanielHajduk98 <danhajduk@protonmail.com>
2021-05-02 18:50:25 +0200

    :rocket: Setup CORS

--- config/cors.php
+++ config/cors.php
@@ -17,9 +17,9 @@ return [
 
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
-    'allowed_methods' => ['*'],
+    'allowed_methods' => ['GET', 'POST'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['https://labkom.herokuapp.com'],
 
     'allowed_origins_patterns' => [],
 
